b8853021ca6a2a856390e442408ee416d94c0348
Shaddy <shadybett540@gmail.com>
2025-01-21 07:45:59 +0300

    made trial on remembered steps

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api,Resource
+from models import User,db
 
 app = Flask(__name__)
 api = Api(app)
+
+password_pattern
+email pattern
+
+class Signup(Resource):
+    @jwt_required
+    def signup(post):
+        id = get_jwt_identity()
+        email = User.query.filter_by(email=email).first()
+        if email:
+            return (error,'email already exists'),500
+        else:
+            new_user = User(
+                'first_name':first_name,
+                'last_name': last_name,
+                'email':email
+                'password':password
+            )
+            db.add(new_user)
+            db.commit(new_user)
 
 if __name__=='__main__':
     app.run(port=5000,debug=True)
--- models.py
+++ models.py
@@ -2,7 +2,8 @@ from flask_sqlalchemy import SQLALCHEMY
 
 db = SQLALCHEMY()
 
-class Users(db.Model):
+class User(db.Model):
+    __tablename__='users',
     "first_name": db.Column(db.String,VarChar(200),nullable=False)
     "last_name": db.Column(db.String,VarChar(200),nullable=False)
     "email": db.Column(db.String,Unique=True,nullable=False)
